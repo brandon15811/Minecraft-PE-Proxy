19c54b82506beb36bd6b5acf24493ae33a2a68c6
Brandon V <brandon15811@gmail.com>
2012-12-16 16:22:01 -0600

    Minecraft PE proxy: Add proper packet filtering/logging

--- mcpeproxy.js
+++ mcpeproxy.js
@@ -3,23 +3,31 @@ var client = dgram.createSocket("udp4");
 var serverip = process.argv[2];
 var serverPort = 19132;
 var ipArray = { };
+var logAllPackets = true;
 client.bind(19132);
 client.setBroadcast(true);
 
+function packetLog(srcip, srcPort, destip, destPort, type)
+{
+    //Only log these packets
+    filter = Array();
+    if (filter.indexOf(type) !== -1 || logAllPackets === true)
+    {
+        console.log("received: 0x" + type + " from " + srcip + ":" + srcPort
+            + ", sending : 0x" + type + " to " + destip + ":" + destPort);
+    }
+}
 client.on("message", function (msg, rinfo) {
+
     type = msg.toString('hex').substr(0,2)
     if (rinfo.address !== serverip)
     {
        var portTime = new Date();
-       ipArray[rinfo.address] = { 'port': rinfo.port, 'ip': rinfo.ip, 'time': portTime.getTime()};
+       ipArray[rinfo.address+":"+rinfo.port] = { 'port': rinfo.port, 'ip': rinfo.ip, 'time': portTime.getTime()};
     }
     if (rinfo.address !== serverip)
     {
-           if (type !== "c0")
-           {
-       console.log("received: 0x" + type + " from " +
-    rinfo.address + ":" + rinfo.port + ", sending : 0x" + type + " to " + serverip + ":" + serverPort);
-           }
+       packetLog(rinfo.address, rinfo.port, serverip, serverPort, type);
        client.send(msg, 0, msg.length, serverPort, serverip);
     }
     else
@@ -27,13 +35,10 @@ client.on("message", function (msg, rinfo) {
        for (key in ipArray)
        {
            if (ipArray.hasOwnProperty(key)) {
-                   if (type !== "c0")
-                   {
-               console.log("received: 0x" + type + " from " +
-    rinfo.address + ":" + rinfo.port + ", sending : 0x" + type + " to " + key + ":" + ipArray[key]['port']);
-                   }
-               client.send(msg, 0, msg.length, ipArray[key]['port'], key);
-               //console.log("Minecraft port for " + key + ": " + ipArray[key]['port']);
+               ip = key.split(":");
+               packetLog(rinfo.address, rinfo.port, ip[0], ipArray[key]['port'], type);
+               //console.log("Minecraft port for " + ip[0] + ": " + ipArray[key]['port']);
+               client.send(msg, 0, msg.length, ipArray[key]['port'], ip[0]);
            }
        }
     }
