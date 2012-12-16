e85a6c22197a966c84ede7a7a48304b2883c87d0
Brandon V <brandon15811@gmail.com>
2012-12-16 16:14:00 -0600

    clean up Minecraft pe proxy indentation, and add .gitignore

--- mcpeproxy.js
+++ mcpeproxy.js
@@ -8,35 +8,33 @@ client.setBroadcast(true);
 
 client.on("message", function (msg, rinfo) {
     type = msg.toString('hex').substr(0,2)
-     if (rinfo.address !== serverip)
-     {
-        var portTime = new Date();
-        ipArray[rinfo.address] = { 'port': rinfo.port, 'ip': rinfo.ip, 'time': portTime.getTime()};
-     }
-     if (rinfo.address !== serverip)
-     {
-            if (type !== "c0")
-            {
-        console.log("received: 0x" + type + " from " +
-     rinfo.address + ":" + rinfo.port + ", sending : 0x" + type + " to " + serverip + ":" + serverPort);
-            }
-        client.send(msg, 0, msg.length, serverPort, serverip);
-     }
-     else
-     {
-        //client.send(msg, 0, msg.length, ipArray[rinfo.address], clientip);
-        for (key in ipArray)
-        {
-            if (ipArray.hasOwnProperty(key)) {
-                    if (type !== "c0")
-                    {
-                console.log("received: 0x" + type + " from " +
-     rinfo.address + ":" + rinfo.port + ", sending : 0x" + type + " to " + key + ":" + ipArray[key]['port']);
-                    }
-                client.send(msg, 0, msg.length, ipArray[key]['port'], key);
-                //console.log("Minecraft port for " + key + ": " + ipArray[key]['port']);
-            } 
-        }
-     }
- });
-
+    if (rinfo.address !== serverip)
+    {
+       var portTime = new Date();
+       ipArray[rinfo.address] = { 'port': rinfo.port, 'ip': rinfo.ip, 'time': portTime.getTime()};
+    }
+    if (rinfo.address !== serverip)
+    {
+           if (type !== "c0")
+           {
+       console.log("received: 0x" + type + " from " +
+    rinfo.address + ":" + rinfo.port + ", sending : 0x" + type + " to " + serverip + ":" + serverPort);
+           }
+       client.send(msg, 0, msg.length, serverPort, serverip);
+    }
+    else
+    {
+       for (key in ipArray)
+       {
+           if (ipArray.hasOwnProperty(key)) {
+                   if (type !== "c0")
+                   {
+               console.log("received: 0x" + type + " from " +
+    rinfo.address + ":" + rinfo.port + ", sending : 0x" + type + " to " + key + ":" + ipArray[key]['port']);
+                   }
+               client.send(msg, 0, msg.length, ipArray[key]['port'], key);
+               //console.log("Minecraft port for " + key + ": " + ipArray[key]['port']);
+           }
+       }
+    }
+});
