96a27309027e55dac1c27c8a867c084bb72b5337
Brandon V <brandon15811@gmail.com>
2012-12-17 14:45:17 -0600

    Add support for multiple devices by binding to a port for each device
    Also added some code from when I was playing with sending packets

--- mcpeproxy.js
+++ mcpeproxy.js
@@ -12,33 +12,57 @@ function packetLog(srcip, srcPort, destip, destPort, type)
     filter = Array("all");
     if (filter.indexOf(type) !== -1 || filter.indexOf("all") !== -1)
     {
-        console.log("received: 0x" + type + " from " + srcip + ":" + srcPort
-            + ", sending : 0x" + type + " to " + destip + ":" + destPort);
+        console.log("received: 0x" + type + " from " + srcip + ":" + srcPort + ", sending : 0x" + 
+            type + " to " + destip + ":" + destPort);
     }
 }
-client.on("message", function (msg, rinfo) {
 
+client.on("message", function(msg, rinfo)
+{
+    packetReceive(msg, rinfo);
+});
+
+function packetReceive(msg, rinfo)
+{
     type = msg.toString('hex').substr(0,2)
     if (rinfo.address !== serverip)
     {
        var portTime = new Date();
-       ipArray[rinfo.address+":"+rinfo.port] = { 'port': rinfo.port, 'ip': rinfo.ip, 'time': portTime.getTime()};
+       ipArray[rinfo.address + ":" + rinfo.port] = { 'port': rinfo.port, 'ip': rinfo.address, 
+           'time': portTime.getTime(), 'socket': dgram.createSocket("udp4")};
+       ipArray[rinfo.address + ":" + rinfo.port].socket.bind(rinfo.port);
+       ipArray[rinfo.address + ":" + rinfo.port].socket.on("message", function(msgg, rinfoo)
+       {
+           packetReceive(msgg, rinfoo);
+       });
+           
     }
     if (rinfo.address !== serverip)
     {
        packetLog(rinfo.address, rinfo.port, serverip, serverPort, type);
-       client.send(msg, 0, msg.length, serverPort, serverip);
+       ipArray[rinfo.address + ":" + rinfo.port].socket.send(msg, 0, msg.length, serverPort, 
+           serverip);
     }
     else
     {
        for (key in ipArray)
        {
            if (ipArray.hasOwnProperty(key)) {
-               ip = key.split(":");
-               packetLog(rinfo.address, rinfo.port, ip[0], ipArray[key]['port'], type);
-               //console.log("Minecraft port for " + ip[0] + ": " + ipArray[key]['port']);
-               client.send(msg, 0, msg.length, ipArray[key]['port'], ip[0]);
+               var currentTime = new Date().getTime();
+               //Measured in milliseconds
+               if ((currentTime - ipArray[key]['time']) > 30000)
+               {
+                    console.log("No packets from " + key + ", removing device");
+                    ipArray[key].socket.close();
+                    delete ipArray[key];
+                    continue;
+               }
+               packetLog(rinfo.address, rinfo.port, ipArray[key]['ip'], ipArray[key]['port'], 
+                   type);
+               //console.log("Minecraft port for " + ipArray[key]['ip'] + ": " + 
+               //    ipArray[key]['port']);
+               client.send(msg, 0, msg.length, ipArray[key]['port'], ipArray[key]['ip']);
            }
        }
     }
-});
+}
---
+++ misc.js
@@ -0,0 +1,21 @@
+//Didn't want to have to delete this stuff
+    if ("06" === type)
+    {
+        serverID = msg.toString('hex').substr(34, 16);
+        console.log("Server ID: " + serverID);
+    }
+    if ("07" === type)
+    {
+        var packetID = "08";
+        var packetMagic = "00ffff00fefefefefdfdfdfd12345678";
+        var packetPort = ("0000" + rinfo.port.toString(16)).slice(-4);
+        var packetMTU = ("0000" + (1492).toString(16)).slice(-4);
+        var packett = new Buffer(packetID + packetMagic + serverID + packetPort + packetMTU + "00", 'hex');
+        //console.log(packetID + "|" + packetMagic + "|" + serverID + "|" + packetPort + "|" + packetMTU + "|" + "00");
+        console.log((packetID + packetMagic + serverID + packetPort + packetMTU + "00").length);
+        console.log(packetPort);
+        //("0800ffff00fefefefefdfdfdfd12345678ffffffffc0ce8a0ac50105b800",'hex');
+        client.send(msg, 0, msg.length, serverPort, serverip);
+        client.send(packett, 0, packett.length, rinfo.port, rinfo.address);
+        console.log("Sending special 0x08 packet to " + rinfo.address + ":" + rinfo.port);
+    }
