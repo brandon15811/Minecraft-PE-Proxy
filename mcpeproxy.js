247d8b878682195874b5b4560d370968b3fe2c48
Brandon V <brandon15811@gmail.com>
2012-12-17 20:30:27 -0600

    Route the packets to the right devices by using the port number

--- mcpeproxy.js
+++ mcpeproxy.js
@@ -9,7 +9,7 @@ client.setBroadcast(true);
 function packetLog(srcip, srcPort, destip, destPort, type)
 {
     //Only log these packets
-    filter = Array("all");
+    filter = Array();
     if (filter.indexOf(type) !== -1 || filter.indexOf("all") !== -1)
     {
         console.log("received: 0x" + type + " from " + srcip + ":" + srcPort + ", sending : 0x" + 
@@ -22,47 +22,41 @@ client.on("message", function(msg, rinfo)
     packetReceive(msg, rinfo);
 });
 
-function packetReceive(msg, rinfo)
+function packetReceive(msg, rinfo, sendPort)
 {
     type = msg.toString('hex').substr(0,2)
     if (rinfo.address !== serverip)
     {
        var portTime = new Date();
-       ipArray[rinfo.address + ":" + rinfo.port] = { 'port': rinfo.port, 'ip': rinfo.address, 
+       ipArray[rinfo.port] = { 'port': rinfo.port, 'ip': rinfo.address, 
            'time': portTime.getTime(), 'socket': dgram.createSocket("udp4")};
-       ipArray[rinfo.address + ":" + rinfo.port].socket.bind(rinfo.port);
-       ipArray[rinfo.address + ":" + rinfo.port].socket.on("message", function(msgg, rinfoo)
+       ipArray[rinfo.port].socket.bind(rinfo.port);
+       ipArray[rinfo.port].socket.on("message", function(msgg, rinfoo)
        {
-           packetReceive(msgg, rinfoo);
+           packetReceive(msgg, rinfoo, ipArray[rinfo.port]['port']);
        });
            
     }
     if (rinfo.address !== serverip)
     {
        packetLog(rinfo.address, rinfo.port, serverip, serverPort, type);
-       ipArray[rinfo.address + ":" + rinfo.port].socket.send(msg, 0, msg.length, serverPort, 
+       ipArray[rinfo.port].socket.send(msg, 0, msg.length, serverPort, 
            serverip);
     }
     else
     {
-       for (key in ipArray)
-       {
-           if (ipArray.hasOwnProperty(key)) {
-               var currentTime = new Date().getTime();
-               //Measured in milliseconds
-               if ((currentTime - ipArray[key]['time']) > 30000)
-               {
-                    console.log("No packets from " + key + ", removing device");
-                    ipArray[key].socket.close();
-                    delete ipArray[key];
-                    continue;
-               }
-               packetLog(rinfo.address, rinfo.port, ipArray[key]['ip'], ipArray[key]['port'], 
-                   type);
-               //console.log("Minecraft port for " + ipArray[key]['ip'] + ": " + 
-               //    ipArray[key]['port']);
-               client.send(msg, 0, msg.length, ipArray[key]['port'], ipArray[key]['ip']);
-           }
-       }
+        var currentTime = new Date().getTime();
+        //Measured in milliseconds
+        if ((currentTime - ipArray[sendPort]['time']) > 30000)
+        {
+             console.log("No packets from " + key + ", removing device");
+             ipArray[sendPort].socket.close();
+             delete ipArray[sendPort];
+        }
+        packetLog(rinfo.address, rinfo.port, ipArray[sendPort]['ip'], ipArray[sendPort]['port'], 
+            type);
+        //console.log("Minecraft port for " + ipArray[sendPort]['ip'] + ": " + 
+        //    ipArray[sendPort]['port']);
+        client.send(msg, 0, msg.length, ipArray[sendPort]['port'], ipArray[sendPort]['ip']);
     }
 }
