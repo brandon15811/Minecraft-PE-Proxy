8fdd7eed42b69c4e7b0e09ed9b3195e8ed693370
Brandon V <brandon15811@gmail.com>
2013-01-24 23:31:40 -0600

    Move packet options to top

--- mcpeproxy.js
+++ mcpeproxy.js
@@ -140,7 +140,7 @@ function packetReceive(msg, rinfo, sendPort)
         ipArray[rinfo.port].socket.send(msg, 0, msg.length, serverPort,
             serverip);
     }
-    //Without this, the proxy will crash if the server acts like a client
+    //Without checking the port, the proxy will crash if the server acts like a client
     else if (rinfo.port == serverPort)
     {
         var currentTime = new Date().getTime();
